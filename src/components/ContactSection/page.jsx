bfcc9630dfe3b83070ddcdeeb9884818b7566e84
Faseeh06 <faseehsafdar06@gmail.com>
2025-08-28 19:03:59 +0500

    Upgrading Block 3 for Contact Section

--- src/components/ContactSection/page.jsx
+++ src/components/ContactSection/page.jsx
@@ -114,13 +114,12 @@ export default function ContactSection() {
                 {/* Right Section - Contact Info */}
                 <div className={styles.rightSection}>
                     <div className={styles.contactCard}>
-                        <div className={styles.locationIcon}>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <div className={styles.topMapIcon}>
+                            <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                 <circle cx="12" cy="10" r="3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </div>
-                        
                         <div className={styles.contactInfo}>
                             <div className={styles.contactItem}>
                                 <div className={styles.phoneIcon}>
